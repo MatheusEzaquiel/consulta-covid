6a2713066738237fdfcfddac27ef8561de28f1c3
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2023-05-19 15:13:41 -0300

    feat(frontend): Componente para aplicação de  máscaras em inputs

---
+++ frontend/src/shared/components/MaskedInput.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent } from "react";
+import  InputMask  from "react-input-mask";
+
+interface ICpfMask {
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    typeMask: string;
+}
+
+
+const onlyNumbers = (str: string) => str.replace(/[^0-9]/g, '');
+
+
+export const MaskedInput = ({value, onChange, typeMask}: ICpfMask) => {
+    
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+
+        onChange({
+            ...e,
+            target: {
+                ...e.target,
+                value: onlyNumbers(e.target.value),
+            }
+        })
+    }
+
+
+    return(
+        <>  
+            <br></br>
+            <InputMask mask={typeMask} value={value} onChange={handleChange} placeholder={typeMask}/> 
+            <br></br>
+        </>
+    );
+}                   
